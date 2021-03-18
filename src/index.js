1c01c008b70e03fe50a9924a2b5c8056f4768cbf
tjdgns5272 <tjdgns5272@naver.com>
2021-03-18 17:02:15 +0900

    add firebase service

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.module.css';
 import App from './app';
+import '@fortawesome/fontawesome-free/js/all.js';
 
 ReactDOM.render(
   <React.StrictMode>
