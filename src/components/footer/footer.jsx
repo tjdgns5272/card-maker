7e0024be1d1541cd8827916f4246a785269988c3
tjdgns5272 <69573484+tjdgns5272@users.noreply.github.com>
2021-04-06 13:12:04 +0900

    channge footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,7 +3,7 @@ import styles from './footer.module.css'
 const Footer = memo(() => {
     return (
         <div className={styles.footer}>
-            <h1 className={styles.footer_text}>Code by SH</h1>
+            <h1 className={styles.footer_text}>Code by JH</h1>
         </div>
         )
 });
